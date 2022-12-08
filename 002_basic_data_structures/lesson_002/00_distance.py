46c1a2d0f88fbc065ce9074cc2ca89b56b746bfd
ddd <daiman4444@gmail.com>
2022-12-08 21:07:21 +0600

    Distance dictionary is complite

--- 002_basic_data_structures/lesson_002/00_distance.py
+++ 002_basic_data_structures/lesson_002/00_distance.py
@@ -12,11 +12,16 @@ sites = {
 # Составим словарь словарей расстояний между ними
 # расстояние на координатной сетке - корень из (x1 - x2) ** 2 + (y1 - y2) ** 2
 
-distances = {}
+moscow_london = round((((sites['Moscow'][0] - sites['London'][0]) ** 2 + (sites['Moscow'][1] - sites['London'][1]) ** 2)**.5), 3)
+moscow_paris = round((((sites['Moscow'][0] - sites['Paris'][0]) ** 2 + (sites['Moscow'][1] - sites['Paris'][1]) ** 2) ** .5), 3)
+london_paris = round((((sites['London'][0] - sites['Paris'][0]) ** 2 + (sites['London'][1] - sites['Paris'][1]) ** 2) ** .5), 3)
 
-moscow = sites[]
 
-# TODO здесь заполнение словаря
+distances = {
+    'Moscow - London': (moscow_london),
+    'London - Paris': (london_paris),
+    'Paris - Moscow': (moscow_paris),
+}
 
 print(distances)
 
