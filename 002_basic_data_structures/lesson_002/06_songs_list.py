7c7259baa28712e9664feb4722faa7d94bac98d1
ddd <daiman4444@gmail.com>
2022-12-09 20:00:14 +0600

    Task complited(TODO removed)

--- 002_basic_data_structures/lesson_002/06_songs_list.py
+++ 002_basic_data_structures/lesson_002/06_songs_list.py
@@ -20,7 +20,7 @@ violator_songs_list = [
 # Обратите внимание, что делать много вычислений внутри print() - плохой стиль.
 # Лучше заранее вычислить необходимое, а затем в print(xxx, yyy, zzz)
 
-# TODO здесь ваш код
+
 
 list_one = (violator_songs_list[3][1], violator_songs_list[5][1], violator_songs_list[8][1])
 
